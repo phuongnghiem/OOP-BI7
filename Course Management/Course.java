413291e5394cf29d9bb6b214a3db8518ecb83e4b
Nguyen D. Mau <34294579+NgDMau@users.noreply.github.com>
2018-01-09 02:16:11 +0700

    Final Course :v

--- Course Management/Course.java
+++ Course Management/Course.java
@@ -3,10 +3,12 @@ public class Course implements java.io.Serializable{
     public String code;
     public String name;
     public int credit;
+    public String lecturer;
 
-    public Course(String NAME, String CODE, int CREDITS){
+    public Course(String NAME, String CODE, int CREDITS,String LECTURER){
         this.name=NAME;
         this.code=CODE;
         this.credit=CREDITS;
+        this.lecturer=LECTURER;
     }
 }
