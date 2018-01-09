9d4bbaa16b78454dbd6902ce457b89dd9514dd92
Nguyen D. Mau <34294579+NgDMau@users.noreply.github.com>
2018-01-09 08:32:08 +0700

    Update TestSerialize.java

--- Course Management/TestSerialize.java
+++ Course Management/TestSerialize.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class TestSerialize {
     public TestSerialize(ArrayList<Lecturer> lec){
         try{
-            FileOutputStream file = new FileOutputStream("dulieu.ser");
+            FileOutputStream file = new FileOutputStream("DataOfLecturerFinal1.ser");
             ObjectOutputStream out = new ObjectOutputStream(file);
 
 
